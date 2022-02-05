606be33abec4e4034cac78a367e784deefa3787b
Your Name <87517930+Sa1D1111@users.noreply.github.com>
2022-02-04 21:39:27 -0800

    token.sol
    Signed-off-by: Your Name <87517930+Sa1D1111@users.noreply.github.com>

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,17 @@
+const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
-module.exports = function(deployer) {
-  deployer.deploy(EthSwap);
-};
+// Deploy Token 
+module.exports = async function(deployer) {
+   await deployer.deploy(Token);
+    const token = await Token.deployed();
+      
+// Deploy EthSwap
+   await deployer.deploy(EthSwap);
+    const ethSwap = await EthSwap.deployed();
+    const ethswap = await EthSwap.deployed();
+
+    // transfer all tokens to EthSwap (1 million)
+    await token.transfer(ethswap.address, '1000000000000000000000000');
+  };
+
