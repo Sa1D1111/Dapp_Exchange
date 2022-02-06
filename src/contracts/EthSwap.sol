2f12a7120a85ad80e19925f966f6b803f7ae6607
Your Name <87517930+Sa1D1111@users.noreply.github.com>
2022-02-06 12:08:48 -0800

    Migrations
    Signed-off-by: Your Name <87517930+Sa1D1111@users.noreply.github.com>

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -5,8 +5,24 @@ import "./Token.sol";
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
     Token public token;
+    uint public rate = 100;
 
     constructor(Token _token) public {
     token = _token;
   }
+
+   function buyTokens() public payable {
+    // Calculate the number of tokens to buy
+    uint tokenAmount = msg.value * rate;
+
+    // // Require that EthSwap has enough tokens
+    // require(token.balanceOf(address(this)) >= tokenAmount);
+
+    // Transfer tokens to the user
+    token.transfer(msg.sender, tokenAmount);
+
+    // // Emit an event
+    // emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+  }
+
 }
